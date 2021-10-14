79d25587e4bb2e6d402b2e1d917c2f84324f360e
Luis Javier Canto Hurtado <javier.canto.h@gmail.com>
2021-10-14 15:06:08 -0500

    API connection and fetching results

--- src/components/Add.js
+++ src/components/Add.js
@@ -1,9 +1,55 @@
-import React from 'react'
+import React, { useState } from 'react';
+
 
 export const Add = () => {
+    // query (search movie)
+    const [query, setQuery] = useState("");
+    // storing the result from the query, returning an array of results
+    const [results, setResults] = useState([]);
+
+    const onChange = (e) => {
+        e.preventDefault();
+        // setting the query = what user is typing
+        setQuery(e.target.value);
+        // fetch result from the movies db
+        fetch(
+          `https://api.themoviedb.org/3/search/movie?api_key=${process.env.REACT_APP_TMDB_KEY}&language=en-US&page=1&include_adult=false&query=${e.target.value}`
+        )
+          .then((res) => res.json())
+          .then((data) => {
+            if (!data.errors) {
+              setResults(data.results);
+            } else {
+              setResults([]);
+            }
+          });
+    };
+
+
     return (
-        <div>
-            <h1>Add Page</h1>
+        <div className="add-page">
+            <div className="container">
+                <div className="add-content">
+                    <div className="input-wrapper">
+                        <input
+                            type="text"
+                            placeholder="Search for a movie"
+                            value={query}
+                            onChange={onChange}
+                        />
+                    </div>
+                    {/* display results */}
+                    {results.length > 0 && (
+                        <ul className="results">
+                            {results.map((movie) => (
+                                <li key={movie.id}>
+                                {movie.title}
+                                </li>
+                            ))}
+                        </ul>
+                    )}
+                </div>
+            </div>
         </div>
     )
 }
