cfa5e6b954d8dd1ee77c15228f1d9f47d87f7dab
Luis Javier Canto Hurtado <javier.canto.h@gmail.com>
2021-10-15 19:32:47 -0500

    Add movie to watchlist and display de movies

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,9 +1,13 @@
-import React from 'react'
+import React from 'react';
 
-export const MovieCard = () => {
+export const MovieCard = ( { movie } ) => {
     return (
-        <div>
-            <h1>MovieCard</h1>
+        <div className="movie-card">
+            <div className="overlay"></div>
+            <img
+                src={`https://image.tmdb.org/t/p/w200${movie.poster_path}`}
+                alt={`${movie.title} Poster`}
+            />
         </div>
     )
 }
--- src/components/Watchlist.js
+++ src/components/Watchlist.js
@@ -1,9 +1,31 @@
-import React from 'react'
+import React, { useContext } from 'react';
+import { GlobalContext } from "../context/GlobalState";
+import { MovieCard } from "./MovieCard";
 
 export const Watchlist = () => {
+    const { watchlist } = useContext(GlobalContext);
+
     return (
-        <div>
-            <h1>Watchlist Page</h1>
+        <div className="movie-page">
+            <div className="container">
+                <div className="header">
+                    <h1 className="heading">My Watchlist</h1>
+                    <span className="count-pill">
+                        {watchlist.length} {watchlist.length === 1 ? "Movie" : "Movies"}
+                    </span>
+                </div>
+                {/* check if there are movies in the watchlist */}
+                {watchlist.length > 0 ? (
+                    <div className="movie-grid">
+                        {/* map the movies */}
+                        {watchlist.map((movie) => (
+                            <MovieCard movie={movie} key={movie.id} type="watchlist" />
+                        ))}
+                    </div>
+                ) : (
+                    <h2 className="no-movies">No movies in your list! Add some!</h2>
+                )}
+            </div>
         </div>
     )
 }
