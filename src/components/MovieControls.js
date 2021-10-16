46d6364bf5d19ec3150a7e0cbbb7001cc01f42b1
Luis Javier Canto Hurtado <javier.canto.h@gmail.com>
2021-10-15 19:57:36 -0500

    Movie cwatched functionality

--- src/components/MovieControls.js
+++ src/components/MovieControls.js
@@ -2,7 +2,7 @@ import React, { useContext } from 'react';
 import { GlobalContext } from "../context/GlobalState";
 
 
-export const MovieControls = ({ type, movie }) => {
+export const MovieControls = ({ movie, type }) => {
     const {
         removeMovieFromWatchlist,
         addMovieToWatched,
@@ -12,37 +12,34 @@ export const MovieControls = ({ type, movie }) => {
 
     return (
         <div className="inner-card-controls">
+            {/* controls for watchlist */}
             {type === "watchlist" && (
                 <>
-                <button className="ctrl-btn" onClick={() => addMovieToWatched(movie)}>
-                    <i className="fa-fw far fa-eye"></i>
-                </button>
-
-                <button
-                    className="ctrl-btn"
-                    onClick={() => removeMovieFromWatchlist(movie.id)}
-                >
-                    <i className="fa-fw fa fa-times"></i>
-                </button>
+                    <button className="ctrl-btn" onClick={() => addMovieToWatched(movie)}>
+                        <i className="fa-fw far fa-eye"></i>
+                    </button>
+                    <button
+                        className="ctrl-btn"
+                        onClick={() => removeMovieFromWatchlist(movie.id)}
+                    >
+                        <i className="fa-fw fa fa-times"></i>
+                    </button>
                 </>
             )}
-
+            {/* controls for watched list */}
             {type === "watched" && (
                 <>
-                <button className="ctrl-btn" onClick={() => moveToWatchlist(movie)}>
-                    <i className="fa-fw far fa-eye-slash"></i>
-                </button>
-
-                <button
-                    className="ctrl-btn"
-                    onClick={() => removeFromWatched(movie.id)}
-                >
-                    <i className="fa-fw fa fa-times"></i>
-                </button>
+                    <button className="ctrl-btn" onClick={() => moveToWatchlist(movie)}>
+                        <i className="fa-fw far fa-eye-slash"></i>
+                    </button>
+                    <button
+                        className="ctrl-btn"
+                        onClick={() => removeFromWatched(movie.id)}
+                    >
+                        <i className="fa-fw fa fa-times"></i>
+                    </button>
                 </>
             )}
         </div>
     )
 }
-
-export default MovieControls;
